e75a91f4f46907e3f4b797a5b93978f119a13509
prashanth <mailboxofprashanth@gmail.com>
2021-08-16 20:41:13 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ConversationListComponent } from './components/conversation-list/conversation-list.component';
+import { ConversationComponent } from './components/conversation/conversation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ConversationListComponent,
+    ConversationComponent
   ],
   imports: [
     BrowserModule,
